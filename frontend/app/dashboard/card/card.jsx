0e5b8feaeef9e648738b6a90c409afe8f74d557a
mydevify <boussaadilouay9@gmail.com>
2024-03-23 07:45:57 +0100

    card

--- frontend/app/dashboard/card/card.jsx
+++ frontend/app/dashboard/card/card.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { LuUsers2 } from "react-icons/lu";
 
 const Card = () => {
   return (
-    <div>Card</div>
+    <div>
+      <div className="card w-64 bg-base-100 shadow-xl">
+  <div className="card-body">
+    <h2 className="card-title"> <LuUsers2 />Total Users</h2>
+    <span>100k</span>
+  </div>
+  <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+</div>
+    </div>
   )
 }
 
