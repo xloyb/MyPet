c38abe072d645c4ae88af44ecf645457d58328fc
mydevify <boussaadilouay9@gmail.com>
2024-03-23 08:00:31 +0100

    Static AdoptionRequest

--- frontend/app/dashboard/AdoptionRequest/AdoptionRequest.tsx
+++ frontend/app/dashboard/AdoptionRequest/AdoptionRequest.tsx
@@ -2,7 +2,131 @@ import React from 'react'
 
 const AdoptionRequest = () => {
   return (
-    <div>AdoptionRequest</div>
+    <div>
+      <div className="overflow-x-auto">
+  <table className="table">
+    {/* head */}
+    <thead>
+      <tr>
+         
+        <th>Name</th>
+        <th>Action</th>
+        <th>Status</th>
+        <th>Date</th>
+      </tr>
+    </thead>
+    <tbody>
+      {/* row 1 */}
+      <tr>
+         
+        <td>
+          <div className="flex items-center gap-3">
+            <div className="avatar">
+              <div className="mask mask-squircle w-12 h-12">
+                <img src="https://daisyui.com/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
+              </div>
+            </div>
+            <div>
+              <div className="font-bold">Hart Hagerty</div>
+              <div className="text-sm opacity-50">United States</div>
+            </div>
+          </div>
+        </td>
+        <td>
+          Zemlak, Daniel and Leannon
+          <br/>
+          <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
+        </td>
+        <td>Pending</td>
+        <th>
+          <button className="btn btn-primary btn-xs">Action</button>
+        </th>
+      </tr>
+      {/* row 2 */}
+      <tr>
+         
+        <td>
+          <div className="flex items-center gap-3">
+            <div className="avatar">
+              <div className="mask mask-squircle w-12 h-12">
+                <img src="https://daisyui.com/tailwind-css-component-profile-3@56w.png" alt="Avatar Tailwind CSS Component" />
+              </div>
+            </div>
+            <div>
+              <div className="font-bold">Brice Swyre</div>
+              <div className="text-sm opacity-50">China</div>
+            </div>
+          </div>
+        </td>
+        <td>
+          Carroll Group
+          <br/>
+          <span className="badge badge-ghost badge-sm">Tax Accountant</span>
+        </td>
+        <td>Done</td>
+        <th>
+          <button className="btn btn-primary btn-xs">Action</button>
+        </th>
+      </tr>
+      {/* row 3 */}
+      <tr>
+         
+        <td>
+          <div className="flex items-center gap-3">
+            <div className="avatar">
+              <div className="mask mask-squircle w-12 h-12">
+                <img src="https://daisyui.com/tailwind-css-component-profile-4@56w.png" alt="Avatar Tailwind CSS Component" />
+              </div>
+            </div>
+            <div>
+              <div className="font-bold">Marjy Ferencz</div>
+              <div className="text-sm opacity-50">Russia</div>
+            </div>
+          </div>
+        </td>
+        <td>
+          Rowe-Schoen
+          <br/>
+          <span className="badge badge-ghost badge-sm">Office Assistant I</span>
+        </td>
+        <td>Pending</td>
+        <th>
+          <button className="btn btn-primary btn-xs">Action</button>
+        </th>
+      </tr>
+      {/* row 4 */}
+      <tr>
+         
+        <td>
+          <div className="flex items-center gap-3">
+            <div className="avatar">
+              <div className="mask mask-squircle w-12 h-12">
+                <img src="https://daisyui.com/tailwind-css-component-profile-5@56w.png" alt="Avatar Tailwind CSS Component" />
+              </div>
+            </div>
+            <div>
+              <div className="font-bold">Yancy Tear</div>
+              <div className="text-sm opacity-50">Brazil</div>
+            </div>
+          </div>
+        </td>
+        <td>
+          Wyman-Ledner
+          <br/>
+          <span className="badge badge-ghost badge-sm">Community Outreach Specialist</span>
+        </td>
+        <td>Cancelled</td>
+        <th>
+          <button className="btn btn-primary btn-xs">Action</button>
+        </th>
+      </tr>
+    </tbody>
+    {/* foot */}
+    
+    
+  </table>
+</div>
+    </div>
   )
 }
 
