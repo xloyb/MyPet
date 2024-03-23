538eea21c821394a41f7e0675c5c706efe7291b4
mydevify <boussaadilouay9@gmail.com>
2024-03-23 23:48:16 +0100

    dynamic search

--- frontend/app/ui/dashboard/search/search.tsx
+++ frontend/app/ui/dashboard/search/search.tsx
@@ -1,3 +1,5 @@
+'use client'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import React from 'react';
 
 interface SearchProps {
@@ -5,9 +7,20 @@ interface SearchProps {
 }
 
 const Search: React.FC<SearchProps> = ({ placeholder }) => {
+    const searchParams = useSearchParams();
+    const { replace } = useRouter();
+    const pathname = usePathname();
+  
+    const handleSearch = (e) => {
+      const params = new URLSearchParams(searchParams);
+  
+      params.set("q", e.target.value);
+  
+      replace(`${pathname}?${params}`);
+    }
   return (
     <div>
-      <input type="text" placeholder={placeholder} className="input input-bordered w-full max-w-xs" />
+      <input type="text" placeholder={placeholder} className="input input-bordered w-full max-w-xs" onChange={handleSearch} />
     </div>
   );
 };
