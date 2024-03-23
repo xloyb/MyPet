ce59b715f8ee6fe3480491a89e799dc69bee194f
mydevify <boussaadilouay9@gmail.com>
2024-03-23 08:17:30 +0100

    next.js structure

--- frontend/app/dashboard/page.jsx
+++ frontend/app/dashboard/page.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-import Card from './card/card'
+import Card from '../ui/card/card'
 import styles from './dashboard.module.css'
-import AdoptionRequest from './AdoptionRequest/AdoptionRequest'
-import Chart from './chart/chart'
-import Rightbar from './rightbar/rightbar'
+import AdoptionRequest from '../ui/AdoptionRequest/AdoptionRequest'
+import Chart from '../ui/chart/chart'
+import Rightbar from '../ui/rightbar/rightbar'
 
 
 const Dashboard = () => {
