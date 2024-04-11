559352c33c366590ee2ec057c1ee47b6a5b53966
mydevify <boussaadilouay9@gmail.com>
2024-04-11 21:08:10 +0100

    "Add condition to allow access to /img even if user not logged in.

--- frontend/auth.config.ts
+++ frontend/auth.config.ts
@@ -8,11 +8,12 @@ export const authConfig = {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-      
+      const isOnImgSrc = nextUrl.pathname.startsWith('/img');
+
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false; 
-      } else if (isLoggedIn) {
+      } else if (isLoggedIn && !isOnImgSrc) {
         return Response.redirect(new URL('/dashboard', nextUrl));
       }
       return true;
