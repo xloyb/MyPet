16742228e477196733a63f572713c0ac2e48cf4c
mydevify <boussaadilouay9@gmail.com>
2024-04-12 15:54:19 +0100

    edits

--- frontend/components/index.tsx
+++ frontend/components/index.tsx
@@ -1,7 +1,6 @@
 import Hero from "./Hero";
 import CustomButton from "./CustomButton";
 import NavBar from "./NavBar";
-import Footer from "./Footer";
 import SearchBar from "./SearchBar";
 import CustomFilter from "./CustomFilter";
 import SearchMenuBreeds from "./SearchMenuBreeds";
@@ -11,7 +10,6 @@ export {
     Hero,
     CustomButton,
     NavBar,
-    Footer,
     SearchBar,
     CustomFilter,
     SearchMenuBreeds,
