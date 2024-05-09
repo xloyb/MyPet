9a25ab7dc30479f0d5501f44eaee49a29f91fe8c
mydevify <boussaadilouay9@gmail.com>
2024-05-09 14:28:35 +0100

    "Add link and button to add lost pet on lost pets page"
    This commit adds a new link and button element to the lost pets page, which directs the user to the add lost pet page when clicked.

--- mypet/app/dashboard/lostpets/page.jsx
+++ mypet/app/dashboard/lostpets/page.jsx
@@ -3,11 +3,18 @@ import styles from "../dashboard.module.css";
 import Lostpet from "@/components/Lostpet";
 import { fetchlostpets } from "@/app/lib/data";
 import { auth } from "@/auth";
+import Link from "next/link";
+
 
 const PetsList = async ({ dashboard }) => {
   const lostpets = await fetchlostpets();
   return (
     <div className={styles.home_container}>
+      <div className={styles.top}>
+        <Link href="/dashboard/lostpets/addlostpet">
+          <button className="btn btn-primary">Add Lost pet</button>
+        </Link>
+      </div>
       <div
         className="content-center grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3"
       >
--- mypet/components/Lostpet.jsx
+++ mypet/components/Lostpet.jsx
@@ -3,6 +3,7 @@ import React from "react";
 
 
 
+
 const Lostpet = ({p}) => {
   return (
     <div
